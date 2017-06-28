aad9baa0405f1e250dc921abe770da8081987550
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 15:02:17 +0000

    changing draft to published, etc

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -21,6 +21,16 @@ class BlogsController < ApplicationController
   def edit
   end
 
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+
+    redirect_to blogs_url, notice: 'Post status has been updated.'
+  end
+  
   # POST /blogs
   # POST /blogs.json
   def create
