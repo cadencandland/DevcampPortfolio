cf317580a4780119af8ad96058562b1742f7d4dc
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 14:44:45 +0000

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    CandlandViewTool::Renderer.copyright 'Caden Candland', 'All rights reserved'
+  end
 end
